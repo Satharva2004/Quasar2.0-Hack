bac512144f4446c4f1956a274841b851b36be928
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2024-03-17 00:51:18 +0530

    summarizer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Forms from './components/form';
 import Navbar from './components/navbar.js';
 import Sidebar from './components/sidebase.js';
-
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Summarizer from './components/summ.js';
 
 function App() {
   return (
     <Router>
       <Navbar></Navbar>
+      
       <div className="App">
         <Sidebar />
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
           <Route path="/forms" element={<Forms />} /> {/* Render Forms component */}
+          <Route path="/summarizer" element={<Summarizer/>} />
         </Routes>
       </div>
     </Router>
--- src/components/sidebase.js
+++ src/components/sidebase.js
@@ -34,12 +34,14 @@ export default function Sidebar(){
           </a>
         </li>
         <li>
+          <Link to="/summarizer">
           <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
             <span class="inline-flex justify-center items-center ml-4">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
             </span>
             <span class="ml-2 text-sm tracking-wide truncate">Summarizer</span>
           </a>
+          </Link> 
         </li>
         <li>
           <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
